75f9b3c4b037445f5676797fdeece4941dbd03a3
shreyas <shreyasmamtha19@gmail.com>
2026-03-29 18:42:13 +0530

    Delete midguard-backend directory

--- midguard-backend/Auth/me.js
+++
@@ -1,8 +0,0 @@
-router.get("/me", authguard, async (req, res) => {
-  const user = await User.findOne({
-    where: { publicId: req.user.publicId },
-    attributes: { exclude: ["password_hash"] },
-  });
-
-  res.json({ success: true, user });
-});
